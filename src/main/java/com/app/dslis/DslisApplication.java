05f2be36f3741177ad7b61772b1688f669dd9ed9
Amanda <89360779+amandaconstante@users.noreply.github.com>
2025-08-18 11:54:08 -0300

    Project created

---
+++ src/main/java/com/app/dslis/DslisApplication.java
@@ -0,0 +1,13 @@
+package com.app.dslis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslisApplication.class, args);
+	}
+
+}
